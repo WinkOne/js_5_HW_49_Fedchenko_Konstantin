554a05b5250bfcd690ac319ba211890eb2681aa8
Konstantin Fedchenko <kostya.fedcenko.gmail.com>
2019-10-31 16:08:47 +0600

    replace task-1.sh

--- task-1.sh
+++ task-1.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+cd ~
 mkdir projects
-  cd projects
-| mkdir my_project
+cd projects
+mkdir my_project
 cd my_project
-| | mkdir directory1
+mkdir directory1
 cd directory1
-| | | mkdir subdirectory1
+mkdir subdirectory1
 cd ..
-| | mkdir directory2
-| | directory3
+mkdir directory2
+mkdir directory3
 cd directory3
-| | | mkdir subdirectory2
+mkdir subdirectory2
